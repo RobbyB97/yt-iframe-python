d8492ec73e7baf494e83c219a88f82bc4e1dca2e
Robert Bergers <bergersr@my.easternct.edu>
2019-03-21 09:22:12 -0400

    import bs4, comments, add channel method

--- yt_iframe/yt_iframe/yt.py
+++ yt_iframe/yt_iframe/yt.py
@@ -1,10 +1,18 @@
 
-def gen(link):
+from bs4 import BeautifulSoup as bs
+
+def video(link):
+    # Link = youtube video url. Return iframe as string
+
     try:
         link = link.split('watch?v=')[1]
         string = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + link + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
     except:
         print('yt_iframe - Error! Not a valid link.')
         string = ''
 
     return string
+
+def channel(link):
+    # https://www.youtube.com/feeds/videos.xml?channel_id= UCBcRF18a7Qf58cCRy5xuWwQ
