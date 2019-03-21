7f37090a9eb32aef62df3bdcc9f37efe968fb78f
Robert Bergers <bergersr@my.easternct.edu>
2019-03-21 09:21:52 -0400

    add README as long description for PyPI

--- yt_iframe/setup.py
+++ yt_iframe/setup.py
@@ -1,9 +1,17 @@
 import setuptools
+from os import path
+
+dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(dir, 'README.md'), encoding='utf-8') as f:
+    desc = f.read()
 
 setuptools.setup(
       name='yt_iframe',
-      version='0.1',
+      version='0.2',
       description='YouTube video iframe generator',
+      long_description=desc,
+      long_description_content_type='text/markdown',
       url='http://github.com/robbyb97/yt-iframe-python',
       author='Robby Bergers',
       author_email='bergersr@my.easternct.edu',
