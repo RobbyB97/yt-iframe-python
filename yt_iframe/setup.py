5c6f8558f74a3c93b509507a24869088195a22cd
Robert Bergers <bergersr@my.easternct.edu>
2019-03-21 14:15:25 -0400

    add dependencies to setup

--- yt_iframe/setup.py
+++ yt_iframe/setup.py
@@ -6,12 +6,16 @@ dir = path.abspath(path.dirname(__file__))
 with open(path.join(dir, 'README.md'), encoding='utf-8') as f:
     desc = f.read()
 
+with open(path.join(dir, 'requirements.txt'), 'r') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
       name='yt_iframe',
       version='0.2',
       description='YouTube video iframe generator',
       long_description=desc,
       long_description_content_type='text/markdown',
+      install_requires=requirements,
       url='http://github.com/robbyb97/yt-iframe-python',
       author='Robby Bergers',
       author_email='bergersr@my.easternct.edu',
