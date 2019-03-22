21cdeb532f820f38c889e8857198e96e3cf599f3
Robert Bergers <bergersr@my.easternct.edu>
2019-03-22 13:37:09 -0400

    add channelDict function

--- yt_iframe/yt_iframe/yt.py
+++ yt_iframe/yt_iframe/yt.py
@@ -50,6 +50,47 @@ def channel(link):
             links.append(entry['href'])
     return links
 
+
+def channelDict(link):
+    # Alternate version of channel() that returns a dictionary
+    # Key = title of video
+    # Value = video link
+    links = {}
+
+    def userURL(link):
+        user = requests.get(link).text
+        soup = bs(user, 'lxml')
+        link = soup.find("link", {"rel":"canonical"})
+        return channelURL(link['href'])
+    def channelURL(link):
+        link = link.split('/channel/')[1]
+        link = 'https://www.youtube.com/feeds/videos.xml?channel_id=' + link
+        return link
+
+    # Get RSS URL from channel URL
+    if '/channel/' in link:
+        xml = channelURL(link)
+    elif '/user/' in link:
+        xml = userURL(link)
+    else:
+        print('yt.channel - Error! Not a valid link')
+
+    # Get RSS feed
+    feed = requests.get(xml).text
+    xmlsoup = bs(feed, "lxml")
+
+    # Create dictionary entries
+    for entry in xmlsoup.findAll('entry'):
+        ytlink = entry.find('link')
+        if '/watch?v=' in ytlink['href']:
+            title = entry.find('title').text
+            ytlink = ytlink['href']
+            links[title] = ytlink
+        else:
+            continue
+    return links
+
+
 def getFrames(links):
     # Convert links list to iframes list
     iframes = []
